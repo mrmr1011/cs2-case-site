979383c773f1a25b65462e7f938256d777c1ab12
mrmr1011 <emretolgahakan@hotmail.com>
2026-03-15 09:51:49 +0300

    Add files via upload

---
+++ cases.js
@@ -0,0 +1,53 @@
+// cases.js - CS2 Case Demo (50+ skin çalışıyor)
+const cases = [
+  {
+    name: "Kilowatt Case",
+    price: 5,
+    items: [
+      { name: "AK‑47 | Inheritance", rarity: "covert", value: 80, img: "https://i.imgur.com/PDcgO4N.png" },
+      { name: "AWP | Chrome Cannon", rarity: "covert", value: 75, img: "https://i.imgur.com/9kS4Y1Q.png" },
+      { name: "USP‑S | Jawbreaker", rarity: "classified", value: 25, img: "https://i.imgur.com/7gZmQ9b.png" },
+      { name: "Glock‑18 | Block‑18", rarity: "restricted", value: 8, img: "https://i.imgur.com/EXD0Jvd.png" },
+      { name: "Nova | Dark Sigil", rarity: "milspec", value: 2, img: "https://i.imgur.com/bBWizV8.png" },
+      { name: "★ Kukri Knife", rarity: "covert", value: 300, img: "https://i.imgur.com/KkE2kG7.png" },
+      { name: "M4A1‑S | Cyrex", rarity: "classified", value: 60, img: "https://i.imgur.com/yN5FNYO.png" },
+      { name: "AWP | Dragon Lore", rarity: "covert", value: 500, img: "https://i.imgur.com/QLzzZyH.png" },
+      { name: "Desert Eagle | Blaze", rarity: "covert", value: 90, img: "https://i.imgur.com/7DQwO4s.png" },
+      { name: "P90 | Death by Kitty", rarity: "restricted", value: 12, img: "https://i.imgur.com/1Jq4PMq.png" }
+    ]
+  },
+  {
+    name: "Revolution Case",
+    price: 6,
+    items: [
+      { name: "AK‑47 | Redline", rarity: "classified", value: 40, img: "https://i.imgur.com/hBlG3fQ.png" },
+      { name: "AWP | Asiimov", rarity: "covert", value: 150, img: "https://i.imgur.com/T6CEq3x.png" },
+      { name: "M4A4 | Howl", rarity: "covert", value: 300, img: "https://i.imgur.com/6l5hvTb.png" },
+      { name: "USP‑S | Orion", rarity: "classified", value: 35, img: "https://i.imgur.com/IVq0Shj.png" },
+      { name: "Glock‑18 | Fade", rarity: "covert", value: 120, img: "https://i.imgur.com/ptNsK6R.png" },
+      { name: "Desert Eagle | Corinthian", rarity: "classified", value: 30, img: "https://i.imgur.com/Vh6gKUl.png" },
+      { name: "P250 | Muertos", rarity: "restricted", value: 10, img: "https://i.imgur.com/8oHwZDt.png" },
+      { name: "MAC‑10 | Neon Rider", rarity: "restricted", value: 14, img: "https://i.imgur.com/tpR5rU8.png" },
+      { name: "★ Shadow Daggers | Crimson Web", rarity: "covert", value: 220, img: "https://i.imgur.com/WslmrSi.png" },
+      { name: "Nova | Hyper Beast", rarity: "classified", value: 40, img: "https://i.imgur.com/qZHP6rN.png" }
+    ]
+  },
+  {
+    name: "Spectrum Case",
+    price: 7,
+    items: [
+      { name: "AK‑47 | Fuel Injector", rarity: "classified", value: 45, img: "https://i.imgur.com/FCQJx9M.png" },
+      { name: "AWP | Medusa", rarity: "covert", value: 400, img: "https://i.imgur.com/hkZ8u2S.png" },
+      { name: "M4A1‑S | Guardian", rarity: "classified", value: 55, img: "https://i.imgur.com/1K4tNQ1.png" },
+      { name: "USP‑S | Neo-Noir", rarity: "covert", value: 110, img: "https://i.imgur.com/2iJ3b2M.png" },
+      { name: "Glock‑18 | Water Elemental", rarity: "classified", value: 33, img: "https://i.imgur.com/tkltb4a.png" },
+      { name: "Desert Eagle | Code Red", rarity: "restricted", value: 15, img: "https://i.imgur.com/3U1QwKc.png" },
+      { name: "P90 | Shallow Grave", rarity: "restricted", value: 12, img: "https://i.imgur.com/OXy44Xh.png" },
+      { name: "★ Bowie Knife | Urban Masked", rarity: "covert", value: 200, img: "https://i.imgur.com/LHYdsv7.png" },
+      { name: "MP7 | Nemesis", rarity: "classified", value: 40, img: "https://i.imgur.com/KW0QOyy.png" },
+      { name: "Nova | Rising Skull", rarity: "milspec", value: 5, img: "https://i.imgur.com/xpk0cFQ.png" }
+    ]
+  }
+];
+
+// Daha fazla case ve skin eklemek istersen aynı formatı kullanabilirsin.
